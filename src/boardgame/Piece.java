0034ed0eec5b8560708a2eb8bac0ac0e0bfd330a
Patrick <patrickolisans@hotmail.com>
2020-04-06 23:19:06 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+public class Piece {
+	protected Position position; //n pode ser visivel na camada de xadrez
+	private Board  board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
+}

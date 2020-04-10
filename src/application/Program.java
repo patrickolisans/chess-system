b01d4fa208e6f393876af1a341fa0dbdc16e76c3
Patrick <patrickolisans@hotmail.com>
2020-04-10 17:51:25 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true){
+			UI.printBoard(chessMatch.getPieces()); // imprime o tabuleiro na tela
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc); //pede a posição de origem
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc); //pede a posição de destino
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,6 +43,19 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position)== null) {
+			return null;
+		}
+		Piece aux = piece(position); // aux do tipo piece recebe a peça da posição informada
+		aux.position = null; // posição da peça aux é null
+		pieces[position.getRow()][position.getColumn()]= null; // posição informada não tem mais peça
+		return aux; // retorna aux que contém a peça que foi retirada
+	}
+	
 	private boolean positionExists (int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
